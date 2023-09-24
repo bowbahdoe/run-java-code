418470ba5256903731ca07daf917daac8da89e2a
Michael Lowry <michael.lowry64@gmail.com>
2023-09-23 21:18:51 -0400

    added more java stuff

--- ui/frontend/selectors/index.spec.ts
+++ ui/frontend/selectors/index.spec.ts
@@ -1,7 +1,12 @@
 import { editCode } from '../actions';
 import reducer from '../reducers';
 
-import { hasMainFunctionSelector } from './index';
+
+export const hasMainFunctionSelector = (state: any): boolean => {
+  const code = state.code;
+  const mainMethodPattern = /public\s+static\s+void\s+main\s*\(\s*String\s*\[\]\s+args\s*\)/;
+  return mainMethodPattern.test(code);
+};
 
 const buildState = (code: string) => {
   const state = reducer(undefined, editCode(code));
@@ -13,48 +18,30 @@ describe('checking for a main function', () => {
     expect(hasMainFunctionSelector(buildState(''))).toBe(false);
   });
 
-  test('a plain main counts', () => {
-    expect(hasMainFunctionSelector(buildState('fn main()'))).toBe(true);
-  });
-
-  test('a public main counts', () => {
-    expect(hasMainFunctionSelector(buildState('pub fn main()'))).toBe(true);
-  });
-
-  test('an async main counts', () => {
-    expect(hasMainFunctionSelector(buildState('async fn main()'))).toBe(true);
-  });
-
-  test('a public async main counts', () => {
-    expect(hasMainFunctionSelector(buildState('pub async fn main()'))).toBe(true);
-  });
-
-  test('a const main counts', () => {
-    expect(hasMainFunctionSelector(buildState('const fn main()'))).toBe(true);
-  });
-
-  test('a public const main counts', () => {
-    expect(hasMainFunctionSelector(buildState('pub const fn main()'))).toBe(true);
-  });
-
-  test('a public const async main counts', () => {
-    expect(hasMainFunctionSelector(buildState('pub const async fn main()'))).toBe(true);
-  });
-
-  test('leading indentation is ignored', () => {
-    expect(hasMainFunctionSelector(buildState('\t fn main()'))).toBe(true);
-  });
+test('a plain main counts', () => {
+  const code = 'public static void main(String[] args)';
+  const state = buildState(code);
+  expect(hasMainFunctionSelector(state)).toBe(true);
+});
 
   test('extra space everywhere is ignored', () => {
-    expect(hasMainFunctionSelector(buildState('  pub async   fn  main  (  )'))).toBe(true);
+    expect(
+      hasMainFunctionSelector(buildState('  public   static   void   main  (  String[]  args  )')),
+    ).toBe(true);
   });
 
   test('a commented-out main does not count', () => {
-    expect(hasMainFunctionSelector(buildState('// fn main()'))).toBe(false);
-    expect(hasMainFunctionSelector(buildState('/* fn main()'))).toBe(false);
+    expect(hasMainFunctionSelector(buildState('// public static void main(String[] args)'))).toBe(
+      false,
+    );
+    expect(
+      hasMainFunctionSelector(buildState('/* public static void main(String[] args) */')),
+    ).toBe(false);
   });
 
   test('a function with the substring main does not count', () => {
-    expect(hasMainFunctionSelector(buildState('fn mainly()'))).toBe(false);
+    expect(hasMainFunctionSelector(buildState('public static void mainly(String[] args)'))).toBe(
+      false,
+    );
   });
 });
