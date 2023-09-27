fa0e985eccd283b16e75769b99aa356010d95206
tk1github <tliu1@knights.ucf.edu>
2023-09-27 15:31:09 -0400

    Menu Change (Run and Build)

--- ui/frontend/BuildMenu.tsx
+++ ui/frontend/BuildMenu.tsx
@@ -41,35 +41,14 @@ const BuildMenu: React.FC<BuildMenuProps> = props => {
   const test = useDispatchAndClose(actions.performTest, props.close);
 
   return (
-    <MenuGroup title="What do you want to do?">
+    <MenuGroup title="Run Now">
       <ButtonMenuItem name="Run" onClick={execute}>
-        Build and run the code, showing the output.
-        Equivalent to <code className={styles.code}>cargo run</code>.
+        Build and run the code, showing the output. 
+        Equivalent to <code className={styles.code}>java Main.java</code>.
       </ButtonMenuItem>
       <ButtonMenuItem name="Build" onClick={compile}>
-        Build the code without running it.
-        Equivalent to <code className={styles.code}>cargo build</code>.
-      </ButtonMenuItem>
-      <ButtonMenuItem name="Test" onClick={test}>
-        Build the code and run all the tests.
-        Equivalent to <code className={styles.code}>cargo test</code>.
-      </ButtonMenuItem>
-      <ButtonMenuItem name="ASM" onClick={compileToAssembly}>
-        Build and show the resulting assembly code.
-      </ButtonMenuItem>
-      <ButtonMenuItem name="LLVM IR" onClick={compileToLLVM}>
-        Build and show the resulting LLVM IR, LLVM’s intermediate representation.
-      </ButtonMenuItem>
-      <ButtonMenuItem name="MIR" onClick={compileToMir}>
-        Build and show the resulting MIR, Rust’s control-flow-based intermediate representation.
-      </ButtonMenuItem>
-      <ButtonMenuItem name="HIR" onClick={compileToHir}>
-        Build and show the resulting HIR, Rust’s syntax-based intermediate representation.
-        {!isHirAvailable && <HirAside />}
-      </ButtonMenuItem>
-      <ButtonMenuItem name="WASM" onClick={compileToWasm}>
-        Build a WebAssembly module for web browsers, in the .WAT textual representation.
-        {!isWasmAvailable && <WasmAside />}
+        Build the code without running it. 
+        Equivalent to <code className={styles.code}>javac Main.java</code>.
       </ButtonMenuItem>
     </MenuGroup>
   );
